30b49fd1cf114c3b3dbb05d2b46345886b5b5810
oscarcalderon402 <69067743+oscarcalderon402@users.noreply.github.com>
2022-06-17 23:36:55 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const config = {
   dbUrl:
-    process.env.DB_URL ||
-    'mongodb+srv://oscarUalett:guitarra2@cluster0.e1gib.mongodb.net/api_numb',
+    process.env.DB_URL,
   jwtSecret: process.env.JWT_SECRET
 };
 
